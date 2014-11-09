d689a267f58fd20f87bab03a21d8b3ff616c1430
stenote <rui.ma@geneegroup.com>
2014-11-09 13:12:40 +0000

    修改手写错误

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ chown -R mysql:mysql /var/lib/mysql
 
 mkdir /var/log/php5/
 touch /var/log/php5/cli.log /var/log/php5/cgi.log
-chown www-data:www-data /var/log/php5/cli/log
+chown www-data:www-data /var/log/php5/cli.log
 chown www-data:www-data /var/log/php5/cgi.log
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
