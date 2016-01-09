f61df69755ac2e3d3b6d4472ee632e8509a450d0
Rui Ma <rui.ma@geneegroup.com>
2016-01-09 11:08:02 +0800

    Fix entrypoint.sh Bug
    We should init MySQL db if necessary

--- files/root/entrypoint.sh
+++ files/root/entrypoint.sh
@@ -3,6 +3,12 @@
 set -e
 
 chown -R www-data:www-data /var/www /var/log/php5
+
+# init mysql db if necessary
+if [ ! -d /var/lib/mysql/mysql ];then
+  mysql_install_db
+fi
+
 chown -R mysql:mysql /var/lib/mysql
 
 exec /usr/bin/supervisord --nodaemon -c /etc/supervisor/supervisord.conf
