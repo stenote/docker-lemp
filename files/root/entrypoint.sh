2c29eccc6c9f0ab04f9e9cce7fd63d8e0e80e1ee
Joshua Haupt <joshua@hauptj.com>
2019-04-27 20:48:03 -0500

    Removed commented out code

--- files/root/entrypoint.sh
+++ files/root/entrypoint.sh
@@ -4,11 +4,4 @@ set -e
 
 chown -R www-data:www-data /var/www /var/log/php
 
-# init mysql db if necessary
-# if [ ! -d /var/lib/mysql/mysql ];then
-#     mysqld --initialize-insecure --user=root --datadir=/var/lib/mysql
-# fi
-
-# chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
-
 exec /usr/bin/supervisord --nodaemon -c /etc/supervisor/supervisord.conf
